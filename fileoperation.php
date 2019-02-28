58da891e14b17f02c6e7531717bedebdc473012c
SanijaPrakash <48078134+SanijaPrakash@users.noreply.github.com>
2019-02-28 09:28:29 +0530

    Create fileoperation.php

---
+++ fileoperation.php
@@ -0,0 +1,23 @@
+<?php
+
+
+//reading
+// $file=fopen("sampletext.txt","r");
+// echo fread($file,filesize("sampletext.txt"));
+// fclose($file);
+
+$file1=fopen("sampletext.txt","w");
+$contents="Hiran Ullasan an MCA student";
+fwrite($file1,$contents);
+
+// $contents="Sanija K an MCA student";
+// fwrite($file1,$contents);
+fclose($file1);
+
+// $file=fopen("sampletext.txt","r");
+// echo fread($file,filesize("sampletext.txt"));
+// fclose($file);
+
+
+
+?> 
